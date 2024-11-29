d984429fb17a02c5eaaaf02c7f1384cd3dc83cd5
Kanos812 <harrison.okane@my.jcu.edu.au>
2024-11-30 08:19:51 +1000

    Refactor code to be closer to original template to solve issues with search

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,22 +1,17 @@
-"""
-CP1404 Practical 10 - Flask Demo
-Integrating Wikipedia API onto a Flask application
-Harrison O'Kane
-"""
-
 from flask import Flask, render_template, request, redirect, url_for, session
-import wikipediaapi
+import wikipedia
 
 app = Flask(__name__)
 # Set the secret key. Keep this really secret:
 app.secret_key = 'IT@JCUA0Zr98j/3yXa R~XHH!jmN]LWX/,?RT'
 
-# Create an instance of the Wikipedia API
-wiki_wiki = wikipediaapi.Wikipedia(language='en', user_agent='YourAppName/1.0 (Your Contact Info)')
-
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return "I am still working on this"
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
@@ -27,18 +22,26 @@ def search():
 
 @app.route('/results')
 def results():
-    search_term = session.get('search_term', None)
-    if search_term:
-        page = get_page(search_term)
-        return render_template("results.html", title=page.title, summary=page.summary[:500], url=page.fullurl)
-    return redirect(url_for('search'))
+    search_term = session['search_term']
+    page = get_page(search_term)
+    return render_template("results.html", page=page)
 
 def get_page(search_term):
-    page = wiki_wiki.page(search_term)
-    if page.exists():
-        return page
-    else:
-        return None
+    try:
+        page = wikipedia.page(search_term)
+    except wikipedia.exceptions.PageError:
+        # no such page, return a random one
+        page = wikipedia.page(wikipedia.random())
+    except wikipedia.exceptions.DisambiguationError as e:
+        # this is a disambiguation page, get the first real page (close enough)
+        page_titles = e.options
+        # sometimes the next page has the same name (different caps), so don't try the same again
+        if page_titles[1].lower() == page_titles[0].lower():
+            title = page_titles[2]
+        else:
+            title = page_titles[1]
+        page = get_page(title)
+    return page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
